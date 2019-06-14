ce5b2b2ee81726f57b2d0eb56280083e95ea8bc8
Toorusr <toorusr@protonmail.com>
2019-06-14 22:32:11 +0200

    Add: query to logging

--- collector.js
+++ collector.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 // return router info on root call and save everything
 router.all('/', (req, res) => {
     res.send("Next time chocolate cookies and not data please, got your data.")
-    fs.writeFile("./data/" + Date.now(), JSON.stringify(req.body), function(err) {
+    fs.writeFile("./data/" + Date.now(), JSON.stringify(req.body + "\n" + req.query), function(err) {
         if(err) {
             return console.log(err);
         }
-        console.log(Date.now() + " => " + JSON.stringify(req.body));
+        console.log(Date.now() + " => " + JSON.stringify(req.body) + "\t" + JSON.stringify(req.query));
     });
 })
 
