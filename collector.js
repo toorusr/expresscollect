dadd115f9dbbb692278f6ef1dde457ee989256dd
Toorusr <toorusr@protonmail.com>
2019-08-04 16:50:03 +0200

    Add: tebot to collector

--- collector.js
+++ collector.js
@@ -2,10 +2,15 @@
 // @toorusr, https://max.berlin
 // Express Collector
 
-// require express and fs
+// require express, fs and tebot
 var express = require('express')
 var router = express.Router()
 var fs = require('fs');
+var tb = require('./tebot');
+
+// load tebot config
+var tb_config = require('./tebot.json')
+tb.load(tb_config.token, tb_config.id)
 
 // return message and save request body + query to data/$timestamp
 router.all('/', (req, res) => {
@@ -24,7 +29,11 @@ router.all('/', (req, res) => {
             return console.log(err);
         }
         // log the reqest data that we saved to the console
-        console.log(Date.now() + " => " + JSON.stringify(req.body) + "\t" + JSON.stringify(req.query));
+        console.log(Date() + " => " + JSON.stringify(req.body) + "\t" + JSON.stringify(req.query));
+
+        // send to telegram chat
+        tb_data = "Time:\n" + Date() + "\nName:\n" + req.body.name + "\nEmail:\n" + req.body.email + "\nReason:\n" + req.body.reason + "\nMessage:\n" + req.body.message
+        tb.sendMessage(tb_data)
     });
 })
 
